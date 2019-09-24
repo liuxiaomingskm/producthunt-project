5e6b571df51950bac6528c402d9cdb10da361fd0
xiaoming liu <xiaoming.liu@sjsu.edu>
2019-09-24 23:54:13 +0000

    tweaks

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -125,11 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'Producthunt/static/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'producthunt/static/')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
