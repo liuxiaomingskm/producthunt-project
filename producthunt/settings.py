1e92917d51e0496a3cc1021af16a5fe529ef3a5a
Apple <appe@AppledeMacBook-Pro.local>
2019-09-24 15:28:59 -0700

    add requirement.txt

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'producthuntdb',
         'USER': 'postgres',
-        'PASSWORD': 'Liu54420322',
+        'PASSWORD': 'django1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
