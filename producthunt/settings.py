ea7e7ddbaa30c27756e03b97e3ff6ed89cbd2e17
Apple <appe@AppledeMacBook-Pro.local>
2019-09-24 15:05:31 -0700

    added local support

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -128,3 +128,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'Producthunt/static/')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
